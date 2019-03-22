4d2bd580a7983f490f58d77fb3261c33fd938be8
Logan <46866721+logbak@users.noreply.github.com>
2019-03-22 15:55:44 -0400

    Update Exit.cs

--- Code Goblins/Exit.cs
+++ Code Goblins/Exit.cs
@@ -8,23 +8,54 @@ namespace Code_Goblins
 {
     public class Exit
     {
-        public int PosX { get; set; }
+       public int PosX { get; set; }
+
         public int PosY { get; set; }
+
         public bool North { get; set; }
+
+        public int NorthID { get; set; }
+
         public bool South { get; set; }
+
+        public int SouthID { get; set; }
+
         public bool East { get; set; }
+
+        public int EastID { get; set; }
+
         public bool West { get; set; }
+
+        public int WestID { get; set; }
 
         public Exit(){}
 
-        public Exit(int posX, int posY, bool north, bool south, bool east, bool west)
+        public Exit(int posX, int posY, bool north,int northID, bool south, int southID, bool east, int eastID, bool west,int westID)
+
         {
+
             PosX = posX;
+
             PosY = posY;
+
             North = north;
+
+            NorthID = northID;
+
             South = south;
+
+            SouthID = southID;
+
             East = east;
+
+            EastID = eastID;
+
             West = west;
+
+            WestID = westID;
+
         }
+
+
     }
 }
